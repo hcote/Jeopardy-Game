f7865748096ccb0ad98a03e28c0a8f1197abc920
hcote <hcote12@gmail.com>
2018-02-08 16:09:53 -0800

    cleaning up messy code, not complete yet; functionality remains the same

--- app.js
+++ app.js
@@ -5,94 +5,112 @@ $(document).ready(function() {
 
 var userOneAnswer;
 var userTwoAnswer;
-var correctAnswer;;
-var answerWorth
+var correctAnswer;
+var answerWorth;
+var timerValue = 10000;
 
 //Put API function in a variable
-  var assignRandomQuestion = function() {($.ajax({
+function assignRandomQuestion() {($.ajax({
     method: "GET",
     url: "http://www.jservice.io/api/random",
 
     success: function(json) {
-  //    if ($('correctAnswer').not(':contains(" ")')) {
-  // console.log("one word answer");
         console.log(json);
         $("section")
           .removeClass("hidden")
-          .html(json[0].question);
+          .html(json[0].question)
         $("table").addClass("dissapear");
-
         responsiveVoice.speak(json[0].question.toString());
         correctAnswer = json[0].answer.toLowerCase();
         answerWorth = json[0].value;
-      // } else {
-      //   console.log("next question - this one is too hard");
-      // };
     },
-
     error: function() {
       console.log("Error. JSON service is temporarily.");
     }
   }))};
 
-
-
-
-
-
-//Assigns random question to each table box
-$("td").on('click', function() {
-  assignRandomQuestion();
+//Enable players to enter and submit answers
+function enablePlayerSubmits() {
   $(".inputText1").removeAttr("disabled");
   $(".inputText2").removeAttr("disabled");
   $(".submit").removeAttr("disabled");
+};
 
+function disablePlayerSubmits() {
+  $(".submit").prop("disabled", "true");
+};
 
-//FYI -- STILL IN The FUNCTION ABOVE
-//After 10 second timer ends. Go back to the board after click on the question
-  setTimeout(function(){
-    console.log("timer done");
-    $("table").removeClass("dissapear");
-    $("section").addClass("hidden");
-    $(".submit").prop("disabled", "true");
-    $(".inputText1").prop("disabled", "true");
-    $(".inputText2").prop("disabled", "true");
-    $('.inputText1').val('');
-    $('.inputText2').val('');
-    // $("#box11").html(''); the td clicked last will have html be empty
-    if (userOneAnswer === correctAnswer || userTwoAnswer === correctAnswer) {
-
-    } else {
-      responsiveVoice.speak("No answers submitted, the correct answer is " + correctAnswer.toString());
-    };
-  }, 1000);
-});
-
-  window.localStorage.setItem("#playerOneScore", "answerWorth");
-
-
-//If the submitted answer = json.answer, add value to player score
-  $("form").on('submit', function(e) {
-  e.preventDefault();
+//Hides question, sets board back to visible
+function backToBoard() {
   $("table").removeClass("dissapear");
   $("section").addClass("hidden");
-});
+};
 
-  $("#userOneForm").on('submit', function() {
-  userOneAnswer = $(".inputText1").val().toLowerCase();
-  if (userOneAnswer === correctAnswer) {
-  console.log(userOneAnswer + " is correct!");
-  $("#playerOneScore").html(answerWorth);
+function setTimer() {
+  setTimeout(function(){
+  backToBoard();
+  $(".submit").prop("disabled", "true");
+  $(".inputText1").prop("disabled", "true");
+  $(".inputText2").prop("disabled", "true");
   $('.inputText1').val('');
   $('.inputText2').val('');
-  $(".submit").prop("disabled", "true");
-  responsiveVoice.speak("Great job player one, you got the correct answer");
-  // $("form").val(''); NEED TO DISABLE OTHER PLAYERS SUBMIT BUTTON AND CLEAR BOTH INPUT TEXT BOXES UPON CORRECT ASNWER SUBMISSION
-} else {
-   console.log(userOneAnswer + " is wrong! - " + answerWorth);
-   $("#playerOneScore").html(-answerWorth);
+  if (userOneAnswer === correctAnswer || userTwoAnswer === correctAnswer) {
+      return;
+  } else {
+    responsiveVoice.speak("No answers submitted, the correct answer is " + correctAnswer.toString());
+  };
+}, timerValue)};
+
+function resetUserOneTextbox() {
    $('.inputText1').val('');
-   $(".submit").prop("disabled", "true");
+};
+
+function resetUserTwoTextbox() {
+   $('.inputText2').val('');
+};
+
+function resetBothUsersTextbox() {
+  $('.inputText1').val('');
+  $('.inputText2').val('');
+};
+
+//Assigns random question to each table box
+//Enables player submit button & text field
+//Timer logic (after 10 seconds: hide question, show board, disable users submit buttons, reset textbox)
+//If user puts correct answer, end function
+//If neither user submits answer, voice responds with correct answer
+$("td").on('click', function() {
+  assignRandomQuestion();
+  enablePlayerSubmits();
+  setTimer();
+});
+
+//When user hits Submit, go back to the board
+$("form").on('submit', function(e) {
+  e.preventDefault();
+  backToBoard();
+});
+
+//When user one hits Submit:
+//Turn submission to lowercase, compare it to correct answer
+//If user one answer is correct, add question value to user one Score
+//Voice responds "you are correct, etc."
+//Reset both players text boxes
+//Disable forms until new question is clicked
+//If user one answer is wrong, doc the question value from user one Score
+//Reset user ones submit button and reset user one textbox
+//Voice responds "you are wrong, etc."
+$("#userOneForm").on('submit', function() {
+  userOneAnswer = $(".inputText1").val().toLowerCase();
+  if (userOneAnswer === correctAnswer) {
+  $("#playerOneScore").html(answerWorth);
+  resetBothUsersTextbox();
+  disablePlayerSubmits();
+  responsiveVoice.speak("Great job player one, you got the correct answer");
+  } else {
+   $("#playerOneScore").html(-answerWorth);
+   resetUserOneTextbox();
+   disablePlayerSubmits();
    if (userOneAnswer === correctAnswer || userTwoAnswer === correctAnswer) {
 
    } else {
@@ -103,17 +121,15 @@ $("td").on('click', function() {
  $("#userTwoForm").on('submit', function() {
  userTwoAnswer = $(".inputText2").val().toLowerCase();
  if (userTwoAnswer === correctAnswer) {
- console.log(userTwoAnswer + " is correct!");
  $("#playerTwoScore").html(answerWorth);
- $('.inputText1').val('');
- $('.inputText2').val('');
- $(".submit").prop("disabled", "true");
+ resetUserOneTextbox();
+ resetUserTwoTextbox();
+ disablePlayerSubmits();
  responsiveVoice.speak("Great job player two, you got the correct answer");
 } else {
-  console.log(userTwoAnswer + " is wrong! - " + answerWorth);
   $("#playerTwoScore").html(-answerWorth);
-  $('.inputText2').val('');
-  $(".submit").prop("disabled", "true");
+  resetUserTwoTextbox();
+  disablePlayerSubmits();;
   if (userOneAnswer === correctAnswer || userTwoAnswer === correctAnswer) {
 
   } else {
@@ -122,21 +138,19 @@ $("td").on('click', function() {
 }});
 
 //Remove box text after being clicked
-$(".box").each(function(index){
-  $(this).on('click', function(index) {
-    $(this).html('');
-  });
-})
+   $(".box").each(function(index){
+   $(this).on('click', function(index) {
+   $(this).html('');
+  })
+});
 
 //End of document.ready
 });
 
 
-
-
 // Challenges
-// Make the number dissapear after question is clicked
-// Make only one word answers
+// Make the number dissapear after question is clicked J
+// Make only one word answers (can't figure out - Kay said regEx)
 // Make it audible for each question J
 // Make a point system J
 // Make it able for two players to play J
