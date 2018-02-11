f89813958f7b035c820f00e680eb995c4781adf7
hcote <hcote12@gmail.com>
2018-02-11 12:50:41 -0800

    finally got each columns questions to align with the randomly assigned column header. big win

--- app.js
+++ app.js
@@ -16,113 +16,179 @@ var answerWorth;
 var timerValue = 10000;
 
 var randomCategories = [];
+var categoryIds = [];
 
 //Randomize Category For Each Column
-$("button").on('click', function getCategory() {($.ajax({
+$("button").on('click', function() {($.ajax({
   method: "GET",
   url: "http://www.jservice.io/api/random",
 
   success: function(json) {
-    randomCategories.push(json[0].category.title);
-    console.log(randomCategories);
+    randomCategories.splice(0, 0, json[0].category.title);
+    categoryIds.splice(0, 0, json[0].category.id);
     $(".CategoryOne").html(randomCategories[0]);
-    //ADD JSON.CATEGORY.ID TO ALL COLUMN ONE BOXES
+    var catIdOne = categoryIds[0];
   },
   error: function() {
     console.log("Category not assigned");
   }
 }))});
-$("button").on('click', function getCategory() {($.ajax({
+$("button").on('click', function() {($.ajax({
   method: "GET",
   url: "http://www.jservice.io/api/random",
 
   success: function(json) {
-    randomCategories.push(json[0].category.title);
-    console.log(randomCategories);
+    randomCategories.splice(1, 0, json[0].category.title);
+    categoryIds.splice(1, 0, json[0].category.id);
     $(".CategoryTwo").html(randomCategories[1]);
+    var catIdTwo = categoryIds[1];
   },
   error: function() {
     console.log("Category not assigned");
   }
 }))});
-$("button").on('click', function getCategory() {($.ajax({
+$("button").on('click', function() {($.ajax({
   method: "GET",
   url: "http://www.jservice.io/api/random",
 
   success: function(json) {
-    randomCategories.push(json[0].category.title);
-    console.log(randomCategories);
+    randomCategories.splice(2, 0, json[0].category.title);
+    categoryIds.splice(2, 0, json[0].category.id);
     $(".CategoryThree").html(randomCategories[2]);
   },
   error: function() {
     console.log("Category not assigned");
   }
 }))});
-$("button").on('click', function getCategory() {($.ajax({
+$("button").on('click', function() {($.ajax({
   method: "GET",
   url: "http://www.jservice.io/api/random",
 
   success: function(json) {
-    randomCategories.push(json[0].category.title);
-    console.log(randomCategories);
+    randomCategories.splice(3, 0, json[0].category.title);
+    categoryIds.splice(3, 0, json[0].category.id);
     $(".CategoryFour").html(randomCategories[3]);
   },
   error: function() {
     console.log("Category not assigned");
   }
 }))});
-$("button").on('click', function getCategory() {($.ajax({
+$("button").on('click', function() {($.ajax({
   method: "GET",
   url: "http://www.jservice.io/api/random",
 
   success: function(json) {
-    randomCategories.push(json[0].category.title);
-    console.log(randomCategories);
+    randomCategories.splice(4, 0, json[0].category.title);
+    categoryIds.splice(4, 0, json[0].category.id);
     $(".CategoryFive").html(randomCategories[4]);
+    console.log(categoryIds);
   },
   error: function() {
     console.log("Category not assigned");
   }
 }))});
 
-//FAKE AJAX REQUEST - IS THIS WHAT I SHOULD DO?????
-$("button").on('click', function getCategory() {($.ajax({
-  method: "GET",
-  url: "http://www.jservice.io/api/category?id="+//categoryID,
+// var catIdOne = categoryIds[0];
+// var catIdTwo = categoryIds[1];
+// var catIdThree = categoryIds[2];
+// var catIdFour = categoryIds[3];
+// var catIdFive = categoryIds[4];
 
-  success: function(json) {
-    randomCategories.push(json[0].category.title);
-    console.log(randomCategories);
-    $(".CategoryOne").html(randomCategories[0]);
-    //ADD JSON.CATEGORY.ID TO ALL COLUMN ONE BOXES
-  },
-  error: function() {
-    console.log("Category not assigned");
-  }
-}))});
-
-//If clicked .box.html($100) == true {retrieve json.value = 100}, etc.
-//Could go crazy and do a random generator button to generate the categories, then returned json question will only choose a question from the category column that was clicked
-function assignQuestion() {($.ajax({
+function assignOneQuestion() {($.ajax({
     method: "GET",
-    url: "http://www.jservice.io/api/random",
+    url: "http://www.jservice.io/api/category?id="+categoryIds[0],
 
     success: function(json) {
         console.log(json);
         $("section")
           .removeClass("hidden")
-          .html(json[0].question)
+          .html(json.clues[0].question)
         $("table").addClass("dissapear");
-        responsiveVoice.speak("Category: " + json[0].category.title.toString());
-        responsiveVoice.speak(json[0].question.toString());
-        correctAnswer = json[0].answer.toLowerCase();
-        answerWorth = json[0].value;
+        responsiveVoice.speak(json.clues[0].question.toString());
+        correctAnswer = json.clues[0].answer.toLowerCase();
+        answerWorth = json.clues[0].value;
+        console.log(categoryIds);
     },
     error: function() {
-      console.log("Error. JSON service is temporarily.");
+      console.log("Error. JSON service is temporarily unavailable.");
     }
   }))};
+function assignTwoQuestion() {($.ajax({
+      method: "GET",
+      url: "http://www.jservice.io/api/category?id="+categoryIds[1],
 
+      success: function(json) {
+          console.log(json);
+          $("section")
+            .removeClass("hidden")
+            .html(json.clues[0].question)
+          $("table").addClass("dissapear");
+          responsiveVoice.speak(json.clues[0].question.toString());
+          correctAnswer = json.clues[0].answer.toLowerCase();
+          answerWorth = json.clues[0].value;
+          console.log(categoryIds);
+      },
+      error: function() {
+        console.log("Error. JSON service is temporarily unavailable.");
+      }
+    }))};
+function assignThreeQuestion() {($.ajax({
+          method: "GET",
+          url: "http://www.jservice.io/api/category?id="+categoryIds[2],
+
+          success: function(json) {
+              console.log(json);
+              $("section")
+                .removeClass("hidden")
+                .html(json.clues[0].question)
+              $("table").addClass("dissapear");
+              responsiveVoice.speak(json.clues[0].question.toString());
+              correctAnswer = json.clues[0].answer.toLowerCase();
+              answerWorth = json.clues[0].value;
+              console.log(categoryIds);
+          },
+          error: function() {
+            console.log("Error. JSON service is temporarily unavailable.");
+          }
+        }))};
+function assignFourQuestion() {($.ajax({
+              method: "GET",
+              url: "http://www.jservice.io/api/category?id="+categoryIds[3],
+
+              success: function(json) {
+                  console.log(json);
+                  $("section")
+                    .removeClass("hidden")
+                    .html(json.clues[0].question)
+                  $("table").addClass("dissapear");
+                  responsiveVoice.speak(json.clues[0].question.toString());
+                  correctAnswer = json.clues[0].answer.toLowerCase();
+                  answerWorth = json.clues[0].value;
+                  console.log(categoryIds);
+              },
+              error: function() {
+                console.log("Error. JSON service is temporarily unavailable.");
+              }
+            }))};
+function assignFiveQuestion() {($.ajax({
+          method: "GET",
+          url: "http://www.jservice.io/api/category?id="+categoryIds[4],
+
+          success: function(json) {
+              console.log(json);
+              $("section")
+                .removeClass("hidden")
+                .html(json.clues[0].question)
+              $("table").addClass("dissapear");
+              responsiveVoice.speak(json.clues[0].question.toString());
+              correctAnswer = json.clues[0].answer.toLowerCase();
+              answerWorth = json.clues[0].value;
+              console.log(categoryIds);
+          },
+          error: function() {
+            console.log("Error. JSON service is temporarily unavailable.");
+          }
+        }))};
 //Enable/disable players to enter and submit answers
 function enablePlayerSubmits() {
   $(".inputText1").removeAttr("disabled");
@@ -177,8 +243,28 @@ function setTimer() {
 //Timer logic (after 10 seconds: hide question, show board, disable users submit buttons, reset textbox)
 //If user puts correct answer, end function
 //If neither user submits answer, voice responds with correct answer
-$("td").on('click', function() {
-  assignQuestion();
+$(".column1").on('click', function() {
+  assignOneQuestion();
+  enablePlayerSubmits();
+  setTimer();
+});
+$(".column2").on('click', function() {
+  assignTwoQuestion();
+  enablePlayerSubmits();
+  setTimer();
+});
+$(".column3").on('click', function() {
+  assignThreeQuestion();
+  enablePlayerSubmits();
+  setTimer();
+});
+$(".column4").on('click', function() {
+  assignFourQuestion();
+  enablePlayerSubmits();
+  setTimer();
+});
+$(".column5").on('click', function() {
+  assignFiveQuestion();
   enablePlayerSubmits();
   setTimer();
 });
